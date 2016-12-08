7bdc937f2a5869b01fd825a899f1a9856b299324
Steven Zhou <steven.zhou@autodesk.com>
2016-12-08 17:15:28 +0800

    disable kafka autostart

--- kafka/scripts/start-kafka.sh
+++ kafka/scripts/start-kafka.sh
@@ -64,4 +64,4 @@ if [ ! -z "$AUTO_CREATE_TOPICS" ]; then
 fi
 
 # Run Kafka
-$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
+#$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
